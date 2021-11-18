34ad0a317c05f3593f7a6c1f256cd438b5b11678
DESKTOP-TTT89NP\Анастасия <n.leksina@bk.ru>
2021-11-18 18:36:14 +0300

    no message

--- security/s_main.py
+++ security/s_main.py
@@ -80,4 +80,5 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 
 async def get_current_active_user(current_user: User = Depends(get_current_user)):  # UserInDB
+    print(current_user)
     return current_user
